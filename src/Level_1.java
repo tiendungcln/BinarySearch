ce4650395f8f75bf9fd2a2d80334e4f5fdabf32e
tiendungcln <tiendung0301cln@gmail.com>
2026-05-19 16:30:45 +0700

    add Level_1 and Level_1_1 used recursive

---
+++ src/Level_1.java
@@ -0,0 +1,86 @@
+public class Level_1 {
+    // Tìm số có tồn tại không
+    // [1, 3, 5, 7, 9]
+    // + Method 1:
+    // Trả về true/false
+    // target = 5
+    // => true
+    // + Method 2:
+    // Trả về index
+    // target = 5
+    // => 2
+    public static boolean containsTarget(int[] arr, int target){
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right){
+
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target){
+                return true;
+            }
+
+            if (arr[mid] < target){
+                left = mid + 1;
+            }
+
+            if (arr[mid] > target){
+                right = mid - 1;
+            }
+
+        }
+
+        return false;
+
+    }
+
+    public static int findTargetIndex(int[] arr, int target){
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right){
+
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target){
+                return mid;
+            }
+
+            if (arr[mid] < target){
+                left = mid + 1;
+            }
+
+            if (arr[mid] > target){
+                right = mid - 1;
+            }
+
+        }
+
+        return -1;
+
+    }
+
+    public static void main(String[] args){
+
+        int[] arr = {1, 3, 5, 7,  9};
+        int target = 5;
+
+        if (containsTarget(arr, target)){
+            System.out.println("Mảng có tồn tại giá trị cần tìm");
+        }else{
+            System.out.println("Mảng không tồn tại giá trị cần tìm");
+        }
+
+        int result = findTargetIndex(arr, target);
+        if (result == -1){
+            System.out.println("Mảng không có giá trị cần tìm");
+        }else{
+            System.out.println("Mảng có chứa giá trị cần tìm tại index: " + result);
+        }
+
+    }
+
+}
---
+++ src/Level_1_1.java
@@ -0,0 +1,83 @@
+public class Level_1_1 {
+    // -- Sử dụng thuật toán đệ quy --
+    // - Chi tiết đệ quy:
+    // + Dùng method để tự gọi chính nó chứ không dùng for hay while
+    // + Phải có điều kiện dừng
+    // Tìm số có tồn tại không
+    // [1, 3, 5, 7, 9]
+    // + Method 1:
+    // Trả về true/false
+    // target = 7
+    // => true
+    // + Method 2:
+    // Trả về index
+    // target = 5
+    // => 2
+
+    public static boolean containsTarget(int[] arr, int left, int right, int target){
+
+        if (left <= right){
+
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target){
+                return true;
+            }
+
+            if (arr[mid] < target){
+                return containsTarget(arr, mid + 1, right, target);
+            }else{
+                return containsTarget(arr, left, mid - 1, target);
+            }
+
+        }
+
+        return false;
+
+    }
+
+    public static int findTargetIndex(int[] arr, int left, int right, int target){
+
+        if (left <= right){
+
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target){
+                return mid;
+            }
+
+            if (arr[mid] < target){
+                return findTargetIndex(arr, mid + 1, right, target);
+            }else{
+                return findTargetIndex(arr, left, mid - 1, target);
+            }
+
+        }
+
+        return -1;
+
+    }
+
+    public static void main(String[] args){
+
+        int[] arr = {1, 3, 5, 7,  9};
+        int target = 5;
+        int r = arr.length - 1;
+
+        boolean result = containsTarget(arr, 0, r, target);
+        if (result){
+            System.out.println("Mảng có tồn tại giá trị cần tìm");
+        }else{
+            System.out.println("Mảng không tồn tại giá trị cần tìm");
+        }
+
+        int resultIndex = findTargetIndex(arr, 0, r, target);
+        if (resultIndex == -1){
+            System.out.println("Mảng không có giá trị cần tìm");
+        }else{
+            System.out.println("Mảng có chứa giá trị cần tìm tại index: " + resultIndex);
+        }
+
+    }
+
+}
