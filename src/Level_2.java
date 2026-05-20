81508ffb9c24b09f494a77b5b461ba130e6e2188
tiendungcln <tiendung0301cln@gmail.com>
2026-05-20 20:17:14 +0700

    add Level_2 and Level_2_1

---
+++ src/Level_2.java
@@ -0,0 +1,46 @@
+public class Level_2 {
+    // Tìm first occurrence
+    // Ví dụ:
+    // [1,2,2,2,5]
+    // target = 2
+    // => 1
+
+    public static int findFirstOccurrence(int[] arr, int target){
+
+        int left = 0;
+        int right = arr.length - 1;
+        int answer = -1;
+
+        while (left <= right){
+
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target){
+                answer = mid;
+                right = mid - 1;
+            }
+
+            if (arr[mid] < target){
+                left = mid + 1;
+            }
+
+            if (arr[mid] > target){
+                right = mid - 1;
+            }
+
+        }
+
+        return answer;
+
+    }
+
+    public static void main(String[] args){
+
+        int[] arr = {1, 2, 2, 2, 5 };
+        int target = 2;
+
+        System.out.println(findFirstOccurrence(arr, target));
+
+    }
+
+}
---
+++ src/Level_2_2.java
@@ -0,0 +1,5 @@
+public class Level_2_2 {
+    // Đếm số lần xuất hiện
+    // Dùng: last - first + 1
+
+}
